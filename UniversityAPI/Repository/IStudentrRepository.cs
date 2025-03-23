a9335ce779d8498faf45b2c24195c2913f43d980
kamel <kamelsamy3@gmail.com>
2025-03-23 16:10:11 +0200

    add StudentrRepository interface and add StudentRepository

---
+++ UniversityAPI/Repository/IStudentrRepository.cs
@@ -0,0 +1,16 @@
+﻿using UniversityAPI.Models;
+
+namespace UniversityAPI.Repository
+{
+    public interface IStudentrRepository
+    {
+        public List<Student> GetAll();
+        public Student GetById(int id);
+        public Student GetByName(string name);
+
+        public void Add(Student student);
+        public void Update(Student student);
+        public void Delete(int id);
+
+    }
+}
